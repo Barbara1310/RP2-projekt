c6cd9d6f1c6843a7613a3767755af95d786c2012
Anđela Konjevod <andela.konjevod@gmail.com>
2021-07-04 10:08:45 +0200

    Dodan kostur projekta (MVC & bootstrap)

---
+++ view/_header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf8">
+    <title><?php if (isset($title)) echo $title;
+            else echo "E-spomenar"; ?></title>
+    <link rel="stylesheet" href="<?php echo __SITE_URL; ?>/static/bootstrap/css/bootstrap.css">
+    <script type="text/javascript" src="<?php echo __SITE_URL; ?>/static/jquery-min.js"></script>
+</head>
+
+<body class="container-fluid ">
+    <br>
+    <br>
+    <h1>Welcome, <?php if (isset($_SESSION["user"])) echo $_SESSION["user"]->getUsername() . "!";
+                    else echo " please login or register!"; ?></h1>
+
+    <?php
+    if (isset($_SESSION["user"])) require "_navBar.php";
