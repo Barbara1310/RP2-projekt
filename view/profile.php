8eac81c7320661998350d5f148e3ca39fd75af50
martina-github <martina.cuklic@gmail.com>
2021-07-11 12:46:26 +0200

    prikaz admin. ovlasti

--- view/profile.php
+++ view/profile.php
@@ -27,9 +27,22 @@ else $user = $_SESSION["user"];
                 </div>
             </div>
             <hr>
+        <div class="row">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Administratorske ovlasti</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    <?php  if($user->is_admin) echo 'Da' ?>
+                    <?php  if(!($user->is_admin)) echo 'Ne' ?>
+
+                </div>
+            </div>
+            <hr>
         </div>
     </div>
 </div>
+
+
 <?php
 require_once __SITE_PATH . '/view/_footer.php';
 ?>
