619c46e99739df21769e1342eb668464006fc5e8
Anđela Konjevod <andela.konjevod@gmail.com>
2021-07-12 23:56:35 +0200

    Dodana slika na landing page

--- view/home.php
+++ view/home.php
@@ -1,4 +1,6 @@
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
+<br><br>
+<img src="<?php echo __SITE_URL ?>/view/spomenar.jpg" class="rounded mx-auto d-block">
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
